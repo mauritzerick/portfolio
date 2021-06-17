d6b02963f46b8e12154a4cfed9c9b2cd711a5030
Mauritz Erick <mauritz.erick@gmail.com>
2021-06-18 06:09:35 +1000

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:314-343-3432">+61 404 063 600</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
           <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+            mauritz.erick@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
