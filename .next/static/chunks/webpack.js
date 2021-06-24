306b0fa8da663659a2fa363f0c92c604e130c963
Mauritz Erick <mauritz.erick@gmail.com>
2021-06-25 01:04:11 +1000

    footer typos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08a853c2481ae7588835"; }
+/******/ 		__webpack_require__.h = function() { return "63b1b8be22a5f7d6f7d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
