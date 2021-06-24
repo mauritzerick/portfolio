e780623c60491a41e817c48fa3d0083bf91a78b7
Mauritz Erick <mauritz.erick@gmail.com>
2021-06-25 02:15:46 +1000

    hide section divider

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7091fe9932a85c1035c1"; }
+/******/ 		__webpack_require__.h = function() { return "2a268bfa842fbf890c0a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import { projects } from '../../constants/constants';
 
 const Projects = () => (
   <Section nopadding id="projects">
-    <SectionDivider />
+    {/* <SectionDivider /> */}
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
       {projects.map((p, i) => {
